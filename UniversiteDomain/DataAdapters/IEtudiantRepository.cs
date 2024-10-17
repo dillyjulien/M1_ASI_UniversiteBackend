4151f875a1152fdbcef5ed1c039e30fd267d98b5
John Doe <johndoe@example.com>
2024-10-17 12:54:44 +0200

    Séance du 17 octobre 2024 finis jusqu'au Chapitre 3 : 7.2

---
+++ UniversiteDomain/DataAdapters/IEtudiantRepository.cs
@@ -0,0 +1,6 @@
+using UniversiteDomain.Entities;
+namespace UniversiteDomain.DataAdapters;
+
+public interface IEtudiantRepository : IRepository<Etudiant>
+{
+}
---
+++ UniversiteDomainTests/EtudiantUnitTests.cs
@@ -0,0 +1,15 @@
+namespace UniversiteDomainTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
